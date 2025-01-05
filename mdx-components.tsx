c93d15623ecea26b6e63b5bb63159ac819a0bc05
rassvet_ii <dekabrist19480705@gmail.com>
2025-01-05 09:18:27 +0900

    Replace about page with MDX

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,6 +2,11 @@ import { MDXComponents } from 'mdx/types';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    wrapper({ children }) {
+      return (<div className="prose prose-slate prose-relative-weight prose-theme-override markdown-body">
+        {children}
+      </div>);
+    },
     ...components,
   };
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,7 @@ const config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './mdx-components.{js,ts,jsx,tsx,mdx}',
   ],
   plugins: [
     typographyPlugin,
