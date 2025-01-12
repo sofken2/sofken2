59e666a267b73b944945916e67386207c4bfd8d6
rassvet_ii <dekabrist19480705@gmail.com>
2025-01-13 07:46:28 +0900

    Adjust styles and fix hover and focus issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,23 @@ import type { Metadata } from 'next';
 import './globals.css';
 import Link from 'next/link';
 import Image from 'next/image';
-import { M_PLUS_1p } from 'next/font/google';
+import { M_PLUS_1p, M_PLUS_1, Source_Sans_3 } from 'next/font/google';
 
 import { logo_dark } from './res/logo';
+import { clsx } from './utils';
 
-const mplus = M_PLUS_1p({
+const mplusp = M_PLUS_1p({
   variable: '--font-mplus-1p',
   subsets: ['latin'],
   weight: ['100', '300', '400', '500', '700', '800', '900'],
+});
+const mplus = M_PLUS_1({
+  variable: '--font-mplus-1',
+  subsets: ['latin'],
+});
+const source = Source_Sans_3({
+  variable: '--font-source-sans',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
@@ -27,18 +36,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja" className={`scroll-container${(development || preview) ? ' has-[#theme-dark:checked]:[color-scheme:dark] has-[#theme-dark:checked]:[--tw-color-scheme:dark] has-[#theme-light:checked]:[color-scheme:light] has-[#theme-light:checked]:[--tw-color-scheme:light]' : ''}`}>
-      <body className={`min-h-screen ${mplus.variable} font-sans font-light antialiased bg-[url('https://picsum.photos/seed/eternalcore/1920/1200?grayscale')] bg-cover`}>
+    <html lang="ja" className={clsx(`scroll-container`, (development || preview) && 'has-[#theme-dark:checked]:[color-scheme:dark] has-[#theme-dark:checked]:[--tw-color-scheme:dark] has-[#theme-light:checked]:[color-scheme:light] has-[#theme-light:checked]:[--tw-color-scheme:light]')}>
+      <body className={`min-h-screen bg-[url('https://picsum.photos/seed/eternalcore/1920/1200?grayscale')] bg-cover font-sans font-light antialiased ${mplus.variable} ${mplusp.variable} ${source.variable}`}>
 
         <header className="flex flex-row fixed inset-x-0 top-0 z-10 text-white backdrop-blur-sm backdrop-brightness-50 border-b-white border-b-2 max-md:flex-col max-md:items-center max-md:not-at-page-top:-translate-y-24 transition-all delay-200 duration-500">
           <Link href="/" className="h-24 md:not-at-page-top:h-16 transition-all delay-200 duration-500">
             <Image src={logo_dark} alt="二部ソフトウェア研究部" className="px-16 py-4 h-full w-fit object-contain md:not-at-page-top:py-2 md:not-at-page-top:px-24 transition-all delay-200 duration-500" />
           </Link>
-          <ul className="nav h-24 ms-auto flex flex-row gap-1 max-md:h-12 *:max-md:flex-1 max-md:w-full border-white max-md:border-t-[1px] not-at-page-top:h-16 transition-all  delay-200 duration-500">
-            <li><Link href="/about">About Us</Link></li>
-            <li><Link href="/news">News</Link></li>
-            <li><Link href="/blog">Blog</Link></li>
-            <li><Link href="/contact">Contact</Link></li>
+          <ul className="h-24 ms-auto flex flex-row gap-1 max-md:h-12 *:max-md:flex-1 max-md:w-full border-white max-md:border-t-[1px] not-at-page-top:h-16 transition-all delay-200 duration-500">
+            {([
+              ['/about', 'About Us'],
+              ['/news', 'News'],
+              ['/blog', 'Blog'],
+              ['/contact', 'Contact'],
+            ] as const).map(([href, name]) => (<li key={href}>
+              <Link href={href} className="block min-w-max h-full px-8 text-center content-center max-md:px-0 max-lg:px-4 text-white hocus:text-black transition-all duration-500 ease-in-out [box-shadow:inset_0_var(--sw)_white] [--sw:-2px] hocus:[--sw:calc(-1*theme('spacing.24'))] max-md:hocus:[--sw:calc(-1*theme('spacing.12'))] device-touch:[--sw:-4px] device-touch:active:[--sw:-1px] device-touch:duration-200">{name}</Link>
+            </li>))}
           </ul>
         </header>
 
@@ -84,46 +97,44 @@ export default function RootLayout({
           </a>
         </footer>
 
-        {preview && <label className="fixed bottom-4 right-4 w-min self-center p-2 bg-[repeating-linear-gradient(135deg,black_0px,black_20px,yellow_20px,yellow_40px)] transition-all duration-500 has-[#preview-tip:checked]:translate-x-full has-[#preview-tip:checked]:opacity-20 cursor-pointer">
-          <input type="checkbox" id="preview-tip" name="hide-preview-tip" className="sr-only" />
-          <span className="flex flex-row items-center self-center bg-slate-600 px-2 py-1 text-white">
-            <span className="icon-material-symbols-light-construction text-4xl me-2"></span>
-            <span className="first-line:text-lg first-line:leading-none text-xs w-min">Preview<br />Under Construction</span>
-          </span>
+        {(preview) && <label className="fixed bottom-4 right-4 w-max self-center p-2 bg-[repeating-linear-gradient(135deg,black_0px,black_20px,yellow_20px,yellow_40px)] transition-all duration-500 has-[#preview-tip:checked]:translate-x-full has-[#preview-tip:checked]:opacity-20 cursor-pointer">
+          <input type="checkbox" id="preview-tip" name="hide-preview-tip" className="absolute inset-0 appearance-none cursor-pointer" />
+          <div className="flex flex-row items-center self-center bg-slate-600 px-2 py-1 text-white">
+            <span className="icon-mslight-construction text-4xl me-2"></span>
+            <div>
+              <div className="text-lg leading-none">Preview</div>
+              <div className="text-xs">Under Construction</div>
+            </div>
+          </div>
         </label>}
 
-        {false && <div className="fixed inset-0 m-auto size-max text-4xl select-none">
+        {(false) && <div className="fixed inset-0 m-auto size-max text-4xl select-none">
           <span className="sm:after:content-['sm'] md:after:content-['md'] lg:after:content-['lg'] xl:after:content-['xl']" />
           <span> / </span>
           <span className="max-sm:after:content-['max-sm'] max-md:after:content-['max-md'] max-lg:after:content-['max-lg'] max-xl:after:content-['max-xl']" />
         </div>}
 
-        {(development || preview) && <div className="fixed inset-y-0 right-4 m-auto size-10 p-1 bg-black text-white bg-opacity-75 [&_input]:sr-only group/theme">
-          <label className="sr-only group-has-[#theme-light:checked]/theme:not-sr-only cursor-pointer">
-            <input type="radio" name="color-scheme" id="theme-system" defaultChecked />
-            <span className="size-8 icon-mslight-brightness-auto-outline"></span>
-          </label>
-          <label className="sr-only group-has-[#theme-system:checked]/theme:not-sr-only cursor-pointer">
-            <input type="radio" name="color-scheme" id="theme-dark" />
-            <span className="size-8 icon-mslight-light-mode-outline"></span>
-          </label>
-          <label className="sr-only group-has-[#theme-dark:checked]/theme:not-sr-only cursor-pointer">
-            <input type="radio" name="color-scheme" id="theme-light" />
-            <span className="size-8 icon-mslight-dark-mode-outline"></span>
-          </label>
+        {(development || preview) && <div className="flex flex-col fixed inset-y-0 right-4 m-auto size-min p-1 bg-black/80 text-white rounded-full -space-y-1 before:absolute before:inset-x-0 before:size-8 before:m-1 before:rounded-full before:bg-white/20 has-[#theme-light:checked]:before:top-0 has-[#theme-system:checked]:before:top-[1.75rem] has-[#theme-dark:checked]:before:top-[3.5rem] before:transition-all">
+          {([
+            ['theme-light', 'icon-mslight-light-mode-outline', 'Light Mode'],
+            ['theme-system', 'icon-mslight-display-settings-outline', 'No Override', true],
+            ['theme-dark', 'icon-mslight-dark-mode-outline', 'Dark Mode'],
+          ] as const).map(([name, icon, desc, checked]) => (
+            <label key={name} className="size-8 p-1.5 rounded-full cursor-pointer relative group">
+              <input type="radio" name="color-scheme" className="absolute inset-0 rounded-full appearance-none" id={name} defaultChecked={checked} />
+              <span className="absolute inset-y-0 -left-2 m-auto px-2 size-max -translate-x-full invisible group-hover:visible bg-black/80 rounded-full text-sm device-touch:group-active:delay-0 device-touch:group-has-[:checked]:delay-1000 select-none">{desc}</span>
+              <span className={`size-full ${icon} group-hover:scale-125 transition-all`} />
+            </label>
+          ))}
         </div>}
 
-        {(development || preview) && <label className="fixed inset-y-0 left-4 m-auto size-max min-w-40 p-1 bg-black text-white bg-opacity-75 transition-all duration-500 has-[#scroll-tip:checked]:-translate-x-full has-[#scroll-tip:checked]:opacity-20 has-[#scroll-tip:checked]:pe-4 cursor-pointer">
-          <input type="checkbox" id="scroll-tip" name="hide-scroll-tip" className="sr-only" defaultChecked />
+        {(development || preview) && <label className="flex flex-col gap-1 fixed inset-y-0 left-4 m-auto size-max min-w-40 p-1 bg-black/80 text-white transition-all duration-500 has-[#scroll-tip:checked]:-translate-x-full has-[#scroll-tip:checked]:opacity-20 has-[#scroll-tip:checked]:pe-4 cursor-pointer">
+          <input type="checkbox" id="scroll-tip" name="hide-scroll-tip" className="absolute inset-0 appearance-none cursor-pointer" defaultChecked />
 
-          <div className="w-full relative after:absolute after:inset-x-0 after:bottom-0 after:h-[1px] after:bg-white after:origin-left after:scale-x-[--scroll-pos]">scroll-position</div>
-          <div className="w-full relative after:absolute after:right-0 after:left-1/2 after:bottom-0 after:h-[1px] after:bg-white after:origin-left after:scale-x-[--scroll-vel]">scroll-velocity</div>
-          <div className="not-scrolling:bg-white not-scrolling:text-black">state: stopped</div>
-          <div className="scrolling:bg-white scrolling:text-black">state: scrolling</div>
-          <div className="scrolling-up:bg-white scrolling-up:text-black">direction: up</div>
-          <div className="scrolling-down:bg-white scrolling-down:text-black">direction:-down</div>
-          <div className="at-page-top:bg-white at-page-top:text-black">scroll pos: top</div>
-          <div className="at-page-bottom:bg-white at-page-bottom:text-black">scroll pos: bottom</div>
+          <div className="w-full relative after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-current after:origin-left after:scale-x-[--scroll-pos]">scroll-position</div>
+          <div className="w-full relative after:absolute after:right-0 after:left-1/2 after:bottom-0 after:h-px after:bg-current after:origin-left after:scale-x-[--scroll-vel]">scroll-velocity</div>
+          <div className="scrolling-up:after:content-['up'] scrolling-down:after:content-['down'] not-scrolling:after:content-['stopped'] scrolling:text-black scrolling:bg-white">scroll dir: </div>
+          <div className="after:content-['middle'] at-page-top:after:content-['top'] at-page-top:text-black at-page-top:bg-white at-page-bottom:after:content-['bottom'] at-page-bottom:text-black at-page-bottom:bg-white">scroll pos: </div>
         </label>}
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,8 @@ import type { Config } from 'tailwindcss';
 import defaultTheme from 'tailwindcss/defaultTheme';
 import typographyPlugin from '@tailwindcss/typography';
 import { getIconCollections, iconsPlugin } from '@egoist/tailwindcss-icons';
-import { type ResolvableTo } from 'tailwindcss/types/config';
-import { type CSSProperties } from 'react';
 import plugin from 'tailwindcss/plugin';
-
-const key = (w: '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900') => {
-  const [name] = Object.entries(defaultTheme.fontWeight ?? {}).find(([, value]) => value === w) ?? ['normal'];
-  return `fontWeight.${name}`;
-};
+import typographyConfig from './typography.config';
 
 const preview = process.env.deployment === 'preview';
 const development = process.env.NODE_ENV !== 'production';
@@ -37,8 +31,24 @@ const config = {
       },
     }),
     plugin(({ addVariant }) => {
-      addVariant('hover', '@media (hover: hover) { &:hover }');
-      addVariant('hocus', '@media (hover: hover) { &:is(:hover, :focus-visible) }');
+      addVariant('hover', [
+        '@media (hover: hover) { &:is(:hover, :focus-visible) }',
+        '@media (hover: none) { &:is(:active, :focus-visible) }',
+      ]);
+      addVariant('group-hover', [
+        '@media (hover: hover) { .group:is(:hover, :focus-visible) & }',
+        '@media (hover: none) { .group:is(:active, :focus-visible) & }',
+      ]);
+      addVariant('hocus', [
+        '@media (hover: hover) { &:is(:hover, :focus-visible) }',
+        '@media (hover: none) { &:focus-visible }',
+      ]);
+      addVariant('group-hocus', [
+        '@media (hover: hover) { .group:is(:hover, :focus-visible) & }',
+        '@media (hover: none) { .group:focus-visible & }',
+      ]);
+      addVariant('device-hover', '@media (hover: hover)');
+      addVariant('device-touch', '@media (hover: none)');
     }),
     plugin(({ addVariant, addBase, addUtilities }) => {
       addBase({
@@ -104,146 +114,14 @@ const config = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-mplus-1p)', ...defaultTheme.fontFamily.sans],
+        sans: ['var(--font-source-sans)', 'var(--font-mplus-1)', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
       },
-      typography: (({ colors, theme }) => ({
-        'theme-override': {
-          css: {
-            '--tw-prose-links': colors.blue[600],
-            '--tw-prose-visited-links': colors.violet[600],
-            '--tw-prose-invert-links': colors.blue[500],
-            '--tw-prose-invert-visited-links': colors.violet[500],
-
-            '--tw-prose-body': colors.slate[800],
-            '--tw-prose-quote-borders': colors.slate[400],
-            '--tw-prose-hr': colors.slate[400],
-            '--tw-prose-bullets': colors.slate[500],
-
-            'ol:not([type])': { listStyleType: 'decimal' },
-            ':is(ul, ol) ol:not([type])': { listStyleType: 'lower-roman' },
-            ':is(ul, ol) :is(ul, ol) ol:not([type])': { listStyleType: 'lower-alpha' },
-            ':is(ul, ol) :is(ul, ol) :is(ul, ol) ol:not([type])': { listStyleType: 'decimal' },
-            'blockquote': {
-              fontStyle: 'inherit',
-              quotes: 'none',
-            },
-            // 'code:not(pre code)': {},
-            'code:not(pre code)::before': {
-              content: '"`"',
-            },
-            'code:not(pre code)::after': {
-              content: '"`"',
-            },
-          },
-        },
-        'invert': {
-          css: {
-            '--tw-prose-visited-links': 'var(--tw-prose-invert-visited-links)',
-          },
-        },
-        'relative-weight': {
-          css: {
-            'a': { fontWeight: 'inherit' },
-            'strong': { fontWeight: 'bolder' },
-            'ol > li::marker': { fontWeight: 'inherit' },
-            'dt': { fontWeight: 'bolder' },
-            'blockquote': { fontWeight: 'inherit' },
-            'h1': { fontWeight: 'bolder' },
-            'h2': { fontWeight: 'bolder' },
-            'h3': { fontWeight: 'bolder' },
-            'h4': { fontWeight: 'bolder' },
-            'kbd': { fontWeight: 'bolder' },
-            'code': { fontWeight: 'inherit' },
-            'pre': { fontWeight: 'inherit' },
-            'thead th': { fontWeight: 'bolder' },
-          },
-        },
-        'themed-weight': {
-          css: {
-            'a': { fontWeight: theme(key('500'), 'inherit') },
-            'strong': { fontWeight: theme(key('600'), 'inherit') },
-            'ol > li::marker': { fontWeight: theme(key('400'), 'inherit') },
-            'dt': { fontWeight: theme(key('600'), 'inherit') },
-            'blockquote': { fontWeight: theme(key('500'), 'inherit') },
-            'h1': { fontWeight: theme(key('800'), 'inherit') },
-            'h1 strong': { fontWeight: theme(key('900'), 'inherit') },
-            'h2': { fontWeight: theme(key('700'), 'inherit') },
-            'h2 strong': { fontWeight: theme(key('800'), 'inherit') },
-            'h3': { fontWeight: theme(key('600'), 'inherit') },
-            'h3 strong': { fontWeight: theme(key('700'), 'inherit') },
-            'h4': { fontWeight: theme(key('600'), 'inherit') },
-            'h4 strong': { fontWeight: theme(key('700'), 'inherit') },
-            'kbd': { fontWeight: theme(key('500'), 'inherit') },
-            'code': { fontWeight: theme(key('600'), 'inherit') },
-            'pre': { fontWeight: theme(key('400'), 'inherit') },
-            'thead th': { fontWeight: theme(key('600'), 'inherit') },
-          },
-        },
-        'DEFAULT': {
-          css: {
-            'a': {
-              color: `var(--tw-prose-links)`,
-            },
-            'a:hover': {
-              textDecoration: 'none',
-            },
-            'a:visited': {
-              color: `var(--tw-prose-visited-links, --tw-prose-links)`,
-            },
-          },
-        },
-      })) satisfies ResolvableTo<TypographyConfig>,
+      typography: typographyConfig,
     },
   },
 } satisfies Config;
 export default config;
-
-type TypographyConfig = Record<'DEFAULT' | string, { css: Partial<TypographyVariables> & Record<string, CSSProperties> }>;
-
-interface TypographyVariables extends CSSProperties {
-  '--tw-prose-body': string | number;
-  '--tw-prose-headings': string | number;
-  '--tw-prose-lead': string | number;
-  '--tw-prose-links': string | number;
-  '--tw-prose-bold': string | number;
-  '--tw-prose-counters': string | number;
-  '--tw-prose-bullets': string | number;
-  '--tw-prose-hr': string | number;
-  '--tw-prose-quotes': string | number;
-  '--tw-prose-quote-borders': string | number;
-  '--tw-prose-captions': string | number;
-  '--tw-prose-kbd': string | number;
-  '--tw-prose-kbd-shadows': string | number;
-  '--tw-prose-code': string | number;
-  '--tw-prose-pre-code': string | number;
-  '--tw-prose-pre-bg': string | number;
-  '--tw-prose-th-borders': string | number;
-  '--tw-prose-td-borders': string | number;
-  '--tw-prose-invert-body': string | number;
-  '--tw-prose-invert-headings': string | number;
-  '--tw-prose-invert-lead': string | number;
-  '--tw-prose-invert-links': string | number;
-  '--tw-prose-invert-bold': string | number;
-  '--tw-prose-invert-counters': string | number;
-  '--tw-prose-invert-bullets': string | number;
-  '--tw-prose-invert-hr': string | number;
-  '--tw-prose-invert-quotes': string | number;
-  '--tw-prose-invert-quote-borders': string | number;
-  '--tw-prose-invert-captions': string | number;
-  '--tw-prose-invert-kbd': string | number;
-  '--tw-prose-invert-kbd-shadows': string | number;
-  '--tw-prose-invert-code': string | number;
-  '--tw-prose-invert-pre-code': string | number;
-  '--tw-prose-invert-pre-bg': string | number;
-  '--tw-prose-invert-th-borders': string | number;
-  '--tw-prose-invert-td-borders': string | number;
-}
-
-interface TypographyVariables extends CSSProperties {
-  '--tw-prose-visited-links': string | number;
-  '--tw-prose-invert-visited-links': string | number;
-}
